970c2b7b03de7cd87bfaaa1c2cf4f673f2525922
Noa Mishraki <noamishraki2@gmail.com>
2022-05-20 17:40:59 +0300

    post and get without todos - check is expenses is an empty list in get and check for a user with no doc in post

--- common/db_adapter.js
+++ common/db_adapter.js
@@ -6,9 +6,18 @@ const Expenses = connection.model('costs', {
     user_id: {type: String}, expenses: {type: Array}, sum: {type: Number}
 })
 
-async function get_all_expenses_by_user_id(user_id) {
+
+async function is_cost_doc_exist(user_id) {
     const doc = await Expenses.findOne({user_id: user_id});
     if (doc == null) {
+        return false
+    }
+    return doc
+}
+
+async function get_all_expenses_by_user_id(user_id) {
+    const doc = await is_cost_doc_exist(user_id);
+    if ((doc === false) || (doc['expenses'].length < 1)) {
         throw new NoExpensesForUser()
     }
     //TODO: if we add a delete expense so we need to add here a check for expenses array len
@@ -22,11 +31,13 @@ async function get_current_sum_by_user_id(user_id) {
 
 
 async function add_new_expense_by_user_id(user_id, expense) {
-    //TODO: check if the user id new, if it is create new doc for him or cretae automaticly doc when user sign in
+    if (await is_cost_doc_exist(user_id) === false) {
+        const doc = new Expenses({user_id: user_id, expense: [], sum: 0})
+        await doc.save()
+    }
     let new_sum = await get_current_sum_by_user_id(user_id)
     new_sum = new_sum + expense.cost
     await Expenses.updateOne({user_id: user_id}, {$push: {expenses: expense}, $set: {sum: new_sum}});
-
 }
 
 
