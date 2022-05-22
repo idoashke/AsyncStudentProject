f67af8f3bb73a16801461487922ae589cf0d33f9
Noa Mishraki <noamishraki2@gmail.com>
2022-05-22 16:53:55 +0300

    post new user plus basic auth with no real user

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import swaggerUi from 'swagger-ui-express';
 import expenses from "./routes/expenses.js";
 import expense from "./routes/expense.js";
 import statistics from "./routes/statistics.js";
+import expressBasicAuth from "express-basic-auth";
+import {authorize} from "./common/auth.js";
+import user from "./routes/User.js";
 
 const require = createRequire(import.meta.url);
 
@@ -15,6 +18,17 @@ const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
+app.use(expressBasicAuth({
+    authorizer: (username, password, cb) => {
+        const userMatches = expressBasicAuth.safeCompare(username, 'demo')
+        const passwordMatches = expressBasicAuth.safeCompare(password, 'Password1')
+        if (userMatches & passwordMatches)
+            return cb(null, true)
+        else
+            return cb(null, false)
+    },
+    authorizeAsync: true,
+}))
 
 
 app.listen(3000, () => {
@@ -24,3 +38,4 @@ app.listen(3000, () => {
 app.use('/expenses/user', expenses)
 app.use('/expense/user', expense)
 app.use('/expenses-statistics/user', statistics)
+app.use('/user', user)
